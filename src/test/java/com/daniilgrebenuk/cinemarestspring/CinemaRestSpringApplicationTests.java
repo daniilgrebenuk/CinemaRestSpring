055636db07a6eed428f1038ebef27090b7b32f61
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-10-27 20:51:24 +0200

    Spring init

---
+++ src/test/java/com/daniilgrebenuk/cinemarestspring/CinemaRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniilgrebenuk.cinemarestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRestSpringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
