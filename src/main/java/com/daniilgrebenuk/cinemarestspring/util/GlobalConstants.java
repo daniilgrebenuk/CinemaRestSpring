9b4bec0634014f12fab79a49bb2ccf10d95fe1b1
Danylo Hrebeniuk <daniilgrebenuk2002@gmail.com>
2022-11-01 23:17:05 +0100

    Change time pattern

--- src/main/java/com/daniilgrebenuk/cinemarestspring/util/GlobalConstants.java
+++ src/main/java/com/daniilgrebenuk/cinemarestspring/util/GlobalConstants.java
@@ -5,7 +5,7 @@ public interface GlobalConstants {
   String CUSTOMER_NAME_REGEX = "[A-ZŻŹĆĄŚĘŁÓŃ][a-zżźćńółęąś]+";
   String CUSTOMER_SURNAME_REGEX = "[A-ZŻŹĆĄŚĘŁÓŃ][a-zżźćńółęąś]+(-[A-ZŻŹĆĄŚĘŁÓŃ][a-zżźćńółęąś]+)?";
 
-  String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd'T'H:mm";
+  String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm";
 
   long EXPIRATION_TIME_IN_HOURS = 2;
 }
